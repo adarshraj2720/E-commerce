5f0617dd3d356986afbbf1e2ee461a4e393ce223
Adarsh Raj <adarshraj2721@gmail.com>
2022-06-22 00:46:29 +0530

    e-commerce

--- modals/product.js
+++ modals/product.js
@@ -5,7 +5,7 @@ mongoose.plugin(slug);
 
 var productSchema = new schema({
     title : {type : String ,required:true} ,
-    quantity : {type : Number , default:1,required:true},
+    quantity : {type : Number , default:1},
     likes : {type : Number , default :0} ,
     price :{type : Number ,required:true},
     commentID :[{type : schema.Types.ObjectId, ref :"Comment"}] ,
